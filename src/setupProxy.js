e2699a8306b18770d915121b6c97b7484f8d7d4e
Triviks <trivikramaraghavan2001@gmail.com>
2024-05-10 11:51:23 +0530

    second commit

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,12 +1,20 @@
+const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function (app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'https://api.us5.datadoghq.com/api/v1', 
-      changeOrigin: true,
-      pathRewrite: { '^/api': '' },
-    })
-  );
-};
+const app = express();
+
+// Proxy endpoint
+app.use(
+  '/api',
+  createProxyMiddleware({
+    target: 'https://api.us5.datadoghq.com/api/v1',
+    changeOrigin: true,
+    pathRewrite: { '^/api': '' },
+  })
+);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
